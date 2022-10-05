29f01803e1c88d972543f7275f3913c76b0dc060
Dieze <shahine.bouabid@lmh.ox.ac.uk>
2022-10-05 17:22:35 +0100

    added exact KRR module

---
+++ src/models/kernel_ridge_regression.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+
+
+class KRR(nn.Module):
+    """Exact Kernel Ridge Regression
+
+    Args:
+        kernel (gpytorch.kernels.Kernel)
+        λ (float): regularisation weight
+    """
+
+    def __init__(self, kernel, λ):
+        super().__init__()
+        self.kernel = kernel
+        self.λ = λ
+
+    def fit(self, X, y):
+        """Fits kernel ridge regression model
+        ```
+            α = (K(X,X) + nλI)^{-1} @ y
+            f(x) = K(x,X) @ α
+        ```
+
+        Args:
+            X (torch.tensor): (n,) or (n, d)
+            y (torch.tensor): (n,)
+        """
+        n = len(y)
+        K = self.kernel(X, X)
+        Kλ = K.add_diag(self.λ * n * torch.ones(n)).evaluate()
+        Kλ_inv = torch.linalg.cholesky_inverse(Kλ)
+        α = Kλ_inv @ y
+        self.register_buffer('X', X)
+        self.register_buffer('α', α)
+
+    def forward(self, x):
+        output = self.kernel(x, self.X) @ self.α
+        return output
